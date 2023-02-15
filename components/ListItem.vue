964b8f3a9700ed42fb8aab7ab0b1df5259d8f8c9
joesobo <soboleskijoe@gmail.com>
2023-02-14 17:03:27 -0800

    stack on front page

---
+++ components/ListItem.vue
@@ -0,0 +1,29 @@
+<template>
+	<li class="flex items-center">
+		<Icon
+			:name="
+				active
+					? 'material-symbols:check-circle-outline-rounded'
+					: 'charm:circle-cross'
+			"
+			size="16"
+			:class="active ? 'text-green-500 dark:text-green-400' : 'text-gray-400'"
+			class="mr-2"
+		/>
+		{{ label }}
+	</li>
+</template>
+
+<script setup lang="ts">
+defineProps({
+	label: {
+		type: String,
+		default: '',
+		required: true,
+	},
+	active: {
+		type: Boolean,
+		default: false,
+	},
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,51 @@
 <template>
 	<div class="my-4 flex flex-col items-center justify-center">
 		<div class="flex items-center">
-			<Icon name="uil:github" size="20" class="text-[#333] dark:text-white" />
+			<Icon name="uil:home" size="32" class="text-[#333] dark:text-white" />
 			<h1 class="ml-4 mb-0">Welcome Home!</h1>
 		</div>
-		<nuxt-img preload src="/lighthouse.png" sizes="sm:100vw md:50vw lg:400px" />
+		<p>
+			This is a small introductory Fullstack application for me to try out a few
+			new technologies.
+		</p>
+
+		<NuxtImg preload src="/lighthouse.png" sizes="sm:100vw md:50vw lg:400px" />
+
+		<h2
+			class="mb-2 text-lg font-semibold text-gray-900 underline dark:text-white"
+		>
+			Stack:
+		</h2>
+
+		<ul class="w-full max-w-lg text-gray-500 dark:text-gray-400">
+			<h3 class="mt-4 text-base font-semibold text-gray-900 dark:text-white">
+				Frontend:
+			</h3>
+			<ListItem label="Nuxt.js" active />
+			<ListItem label="Vue 3" active />
+			<ListItem label="TailwindCSS / Flowbite" active />
+
+			<h3 class="mt-4 text-base font-semibold text-gray-900 dark:text-white">
+				Testing:
+			</h3>
+			<ListItem label="Vitest" active />
+			<ListItem label="Testing Library" active />
+			<ListItem label="Playwright" active />
+			<ListItem label="CircleCI" active />
+
+			<h3 class="mt-4 text-base font-semibold text-gray-900 dark:text-white">
+				Backend:
+			</h3>
+			<ListItem label="TRPC (Nuxt)" active />
+			<ListItem label="Prisma" active />
+			<ListItem label="Zod" active />
+
+			<h3 class="mt-4 text-base font-semibold text-gray-900 dark:text-white">
+				Database / Deploy:
+			</h3>
+			<ListItem label="Supabase" active />
+			<ListItem label="Vercel" active />
+		</ul>
 	</div>
 </template>
 
